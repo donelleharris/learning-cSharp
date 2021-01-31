9f48cede44c9ae6013e41d1e50801fc0d26673c5
Donelle Harris <donelle.harris@gmail.com>
2021-01-31 11:17:48 -0600

    Continue C# lesson with while & for loops
    will be commented out for next lesson

--- learning-cSharp/Program.cs
+++ learning-cSharp/Program.cs
@@ -6,26 +6,57 @@ namespace learningcSharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
+            //LinkedIn Learning C# For & While Loops
 
-            Console.WriteLine("How old are you?");
-            var age = Console.ReadLine();
-
-            Console.WriteLine("What month were you born?");
-            var birthMonth = Console.ReadLine();
-
-            Console.WriteLine("Your name: {0}", name);
-            Console.WriteLine("Your age: {0}", age);
-            Console.WriteLine("Your birth month: {0}", birthMonth);
-
-            if(birthMonth == "November")
+            var count = 1;
+            while (count <= 5)
             {
-                Console.WriteLine("Your zodiak sign is Scorpio.");
-            } else if (birthMonth == "October")
-            {
-                Console.WriteLine("Your zodiak sign is Libra.");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                for(int j = 10; j > 0; j--)
+                {
+                    Console.WriteLine(j);
+                }
+                count++;
             }
+
+            //LinkedIn Learning C# Passcode & while loop
+            //var code = "";
+            //while (code != "secret")
+            //{
+            //    Console.WriteLine("Please enter the passcode: ");
+            //    code = Console.ReadLine();
+
+            //    if(code != "secret"){
+            //        Console.WriteLine("Not Authenticated");
+            //    }
+            //}
+
+            //Console.WriteLine("Authenticated");
+
+            // LinkedIn Learning C# 
+            //Console.WriteLine("What is your name?");
+            //var name = Console.ReadLine();
+
+            //Console.WriteLine("How old are you?");
+            //var age = Console.ReadLine();
+
+            //Console.WriteLine("What month were you born?");
+            //var birthMonth = Console.ReadLine();
+
+            //Console.WriteLine("Your name: {0}", name);
+            //Console.WriteLine("Your age: {0}", age);
+            //Console.WriteLine("Your birth month: {0}", birthMonth);
+
+            //if(birthMonth == "November")
+            //{
+            //    Console.WriteLine("Your zodiak sign is Scorpio.");
+            //} else if (birthMonth == "October")
+            //{
+            //    Console.WriteLine("Your zodiak sign is Libra.");
+            //}
         }
     }
 }
