96c1351bd1fc901ec8ff4994b53a23db3af15de5
Donelle Harris <donelle.harris@gmail.com>
2021-01-31 10:52:21 -0600

    Implement user input in terminal after prompt.
    will be commented out for next lesson

--- learning-cSharp/Program.cs
+++ learning-cSharp/Program.cs
@@ -6,7 +6,26 @@ namespace learningcSharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born?");
+            var birthMonth = Console.ReadLine();
+
+            Console.WriteLine("Your name: {0}", name);
+            Console.WriteLine("Your age: {0}", age);
+            Console.WriteLine("Your birth month: {0}", birthMonth);
+
+            if(birthMonth == "November")
+            {
+                Console.WriteLine("Your zodiak sign is Scorpio.");
+            } else if (birthMonth == "October")
+            {
+                Console.WriteLine("Your zodiak sign is Libra.");
+            }
         }
     }
 }
